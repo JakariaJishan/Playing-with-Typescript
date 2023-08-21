dbfb10c9123b24c508b6801c166b0d6b634670a0
JakariaJishan <jakariajishan987@gmail.com>
2023-08-22 00:27:09 +0600

    Type assertion

--- src/components/Posts/Auth.tsx
+++ src/components/Posts/Auth.tsx
@@ -4,7 +4,7 @@ type AuthType = {
   email: string;
 };
 const Auth = () => {
-  const [user, setUser] = useState<AuthType | null>(null);
+  const [user, setUser] = useState<AuthType>({} as AuthType);
 
   const handleSignIn = () => {
     setUser({
@@ -12,14 +12,11 @@ const Auth = () => {
       email: "jasdf@mail.com",
     });
   };
-  const handleLogOut = () => {
-    setUser(null);
-  };
+
   return (
     <div>
       <button onClick={handleSignIn}>sign in</button>
-      <button onClick={handleLogOut}>log out</button>
-      <p>welcome: {user?.name}</p>
+      <p>welcome: {user.name}</p>
     </div>
   );
 };
