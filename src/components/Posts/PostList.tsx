5c2a7a7c4394916145d2a7cb101cf2fe95265b02
JakariaJishan <jakariajishan987@gmail.com>
2023-08-21 13:51:41 +0600

    Type for UseState

--- src/components/Posts/PostList.tsx
+++ src/components/Posts/PostList.tsx
@@ -1,10 +1,15 @@
+import { useState } from "react";
 import PostCard from "./PostCard";
 
 const PostList = () => {
+  type status = "success" | "pending" | "failure";
+  const [value, setValue] = useState<status>("success");
+
   return (
     <div>
       data
       <PostCard title="this is title" disabled={true} />
+      <button onClick={() => setValue("failure")}>change status</button>
     </div>
   );
 };
