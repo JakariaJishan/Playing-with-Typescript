047c63e78fb6211e36d1bbd9fbf406563f7244d5
JakariaJishan <jakariajishan987@gmail.com>
2023-08-21 13:57:35 +0600

    Types for DOM element

--- src/components/Posts/PostList.tsx
+++ src/components/Posts/PostList.tsx
@@ -4,12 +4,18 @@ import PostCard from "./PostCard";
 const PostList = () => {
   type status = "success" | "pending" | "failure";
   const [value, setValue] = useState<status>("success");
+  const [inputVal, setInputVal] = useState("change me");
 
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputVal(e.target.value);
+  };
   return (
     <div>
       data
       <PostCard title="this is title" disabled={true} />
       <button onClick={() => setValue("failure")}>change status</button>
+      <input value={inputVal} onChange={handleChange} />
+      <p>{inputVal}</p>
     </div>
   );
 };
